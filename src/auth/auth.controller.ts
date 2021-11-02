dddde3364327b68b76337b527ac0a14e1a289aef
skydrol <keineleben@gmail.com>
2021-11-01 22:39:40 -0300

    Login and protection working

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,27 +1,19 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { stringify } from 'querystring';
-import { User } from 'src/users/user.interface';
 import { AuthService } from './auth.service';
-import { AuthenticatedGuard } from './authenticated.guard';
 import { JwtAuthGuard } from './jwt-auth.guard';
-import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
-  //Post /login
-  //@UseGuards(LocalAuthGuard)
+  
   @Post('login')
   async login(@Request() request) {
     return this.authService.login(request.body.user);  
   }
-
-
-  // GET /protected
+ 
   @UseGuards(JwtAuthGuard)
   @Get('protected')
-  getHello(@Request() request): string { // TODO: require a Bearer Token and validate token
+  getHello(@Request() request): string { 
     return request.body.user; 
   }
 
