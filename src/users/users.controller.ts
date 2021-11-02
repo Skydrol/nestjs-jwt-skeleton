a343dfd84b56a1219334e3f1c4581587e20b3833
skydrol <keineleben@gmail.com>
2021-11-02 00:41:59 -0300

    Login and protection working

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,43 +1,44 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
-    
+  @Post('create')
+  async create(@Body() createUserDto: CreateUserDto) {    
     const saltOrRounds = 10;   
     createUserDto.password = await bcrypt.hash(createUserDto.password, saltOrRounds);
     return this.usersService.create(createUserDto);
-  }
-
-  @Post('user')
-  async login(@Body() createUserDto: CreateUserDto){    
-    return this.usersService.findOneByEmail(createUserDto.email);
-  }
-
-  @Get()
+  } 
+  
+  @Get('all')
   findAll() {
     return this.usersService.findAll();
   }
 
-  @Get(':id')
+  @Get('user/:id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Patch('user/:id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
-  @Delete(':id')
+  @Delete('user/:id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
+  }  
+
+  @Get('profile')
+  async getUserProfile(@Request() request: any) {
+    return request.user;
   }
 }
